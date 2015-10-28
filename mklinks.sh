8116e97e735e2ebf4d8c155c153fb692a31483f7
Darren Kavanagh <dkav@users.noreply.github.com>
2015-10-28 13:19:15 -0700

    Read dot folder contents and copy files
    Remove hardcoding of dot files; get dynamically from folder instead. No
    longer create symlinks but copy dot files. Add timestamp to backup
    folder name.

--- mklinks.sh
+++ mklinks.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-##########################################################################
-# This script creates symlinks from the home directory to dotfiles folder
-##########################################################################
+
+#############################################################
+# Backup existing dot files and copy new files to home folder
+#############################################################
 
 echo
 
+rtime=`date +%Y%m%d%H%M%S`
+
 # Setup directory variables
-dotdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )  # dotfiles directory
-backdir=~/.dotfiles_bak  # dotfiles backup directory
+dotdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+backdir=~/.dotfiles_$rtime
 
 # Assign list of files/folders to symlink in homedir
-files="bash_profile bashrc gitconfig gitignore_global hgignore_global hgrc inputrc 
-       vimrc gvimrc zprofile zshenv zshrc"    
-
+shopt -s extglob
+files=( +([^.]) )
 
 # Create backup directory
 echo "Creating backup folder:"
 echo "$backdir"
-mkdir -p $backdir
-echo
+
+mkdir -p "$backdir"
 
 # Change to the dotfiles directory
-echo "Changing to dotfiles directory:"
-echo "$dotdir"
 cd $dotdir
 echo
 
 # Move dotfiles in homedir to backup directory, then create symlinks 
-echo "Backing up and creating symlinks for the following files:"
-for file in $files; do
+echo "Backing up and/or copying the following dot files:"
+for file in "${files[@]}"; do
     echo ".$file"
     mv ~/.$file $backdir
-    ln -s $dotdir/$file ~/.$file
+    cp $dotdir/$file ~/.$file
 done
