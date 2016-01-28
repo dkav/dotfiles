637b572f22bd81eb7c4139dd378c7bd63a043a51
Darren Kavanagh <dkav@users.noreply.github.com>
2016-01-27 16:39:48 -0800

    Move dot files to folders and add vim plugin script
    Organize dot files in folders. Add new vim plugin script to vim folder.

---
+++ vim/vim_plugins.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install Vim plugins and manage with pathogen #
+#==============================================#
+
+dotdir=~
+autoldir=$dotdir/.vim/autoload
+bundledir=$dotdir/.vim/bundle
+
+echo "Removing old .vim folder"
+rm -frd $dotdir/.vim
+
+mkdir -p $autoldir $bundledir
+
+# Install vim-pathogen
+echo "Installing vim-pathogen"
+curl -LSso $autoldir/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install Vim plugins
+clone_vplugin()
+{   # Function to clone Vim plugins
+    local vim_plugin=$1
+    local github_user=$2
+
+    echo "Installing $vim_plugin"
+    git clone --depth=1 --quiet \
+        https://github.com/$github_user/$vim_plugin.git \
+        $bundledir/$vim_plugin
+    rm -rf !$/.git
+}
+
+clone_vplugin vim-airline bling
+clone_vplugin vim-colors-solarized altercation
+clone_vplugin vim-fugitive tpope
+clone_vplugin vim-flake8 nvie
+
+echo "Done"
