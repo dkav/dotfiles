6455f3ddc5e65fbb8bf247f33425d43a9a3e8f61
Darren Kavanagh <dkav@users.noreply.github.com>
2023-10-16 14:55:22 -0700

    Disable iPython automatic suggestions

--- python/ipython_config.py
+++ python/ipython_config.py
@@ -3,5 +3,4 @@
 c = get_config()  #noqa
 
 # Turn off auto suggestions
-# Broken in 8.9.0
-# c.TerminalInteractiveShell.autosuggestions_provider = None
+c.TerminalInteractiveShell.autosuggestions_provider = None
