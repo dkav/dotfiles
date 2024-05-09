77b5faf137af698515e3a4264e2a880b962a2895
Darren Kavanagh <dkav@users.noreply.github.com>
2024-05-09 13:59:12 -0700

    Add Vi bindings

--- python/ipython_config.py
+++ python/ipython_config.py
@@ -1,5 +1,8 @@
 # Configuration file for ipython.
 c = get_config()  #noqa
 
+# Vim key bindings
+c.TerminalInteractiveShell.editing_mode = "vi"
+
 # Turn off auto suggestions
 c.TerminalInteractiveShell.autosuggestions_provider = None
